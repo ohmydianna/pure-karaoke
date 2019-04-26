27ae0204874f56c1f50d0d5af07458e78aa30f13
ohmydianna <50000634+ohmydianna@users.noreply.github.com>
2019-04-25 22:02:49 -0400

    first commit

---
+++ pure karaoke/menu/menu.js
@@ -0,0 +1,9 @@
+$(document).on(function(){
+     $(".acc-countainer").mouseenter(function(){
+        $("table").show();
+          }),
+    
+    $(".acc-container").mouseover(function(){
+        $("table").hide();
+    });
+}) ;
